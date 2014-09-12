1840bfcda869adb456e4a7b6aa7f613721bedfca
Paul Hutchinson <paul@teamhutch.co.uk>
2014-09-12 14:37:32 -0500

    alter schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911212448) do
+ActiveRecord::Schema.define(:version => 20140912193501) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -53,10 +53,10 @@ ActiveRecord::Schema.define(:version => 20140911212448) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
