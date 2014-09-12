5c2b49e6755d22a8037f3ed772a126d04bc3f6dc
Paul Hutchinson <paul@teamhutch.co.uk>
2014-09-12 08:40:08 -0500

    retriving User files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911151824) do
+ActiveRecord::Schema.define(:version => 20140911212448) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
